638a8d20d6ee0df449f666cc79cd96e41ca600ef
hillarys <hcr901206@gmail.com>
2022-05-30 12:28:11 -0400

    cambio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './page/about-us/about-us.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { HomeComponent } from './page/home/home.component';
+import { ShopComponent } from './page/shop/shop.component';
+import { ShoppingCartComponent } from './page/shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'shopping', component: ShoppingCartComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { ShopComponent } from './page/shop/shop.component';
+import { ContactComponent } from './page/contact/contact.component';
+import { ShoppingCartComponent } from './page/shopping-cart/shopping-cart.component';
+import { AboutUsComponent } from './page/about-us/about-us.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    ContactComponent,
+    ShoppingCartComponent,
+    AboutUsComponent,
+    HeaderComponent,
+    MatToolbarModule,
+    MatIconModule
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
